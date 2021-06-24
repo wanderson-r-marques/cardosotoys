a8bb68dac17e595ea1e87b00859b5a256062c593
Wanderson Rodrigues Marques <32652021+wanderson-r-marques@users.noreply.github.com>
2021-06-24 10:41:52 -0300

    Enviado as imagens MOBILE

---
+++ mobile/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Cardoso Toys</title>
+	<link href="styles.css" rel="stylesheet" type="text/css">
+	<link href="styles2.css" rel="stylesheet" type="text/css">
+	<link href="styles3.css" rel="stylesheet" type="text/css">
+	<link href="styles4.css" rel="stylesheet" type="text/css">
+	<link href="stylesMenu.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" type="image/png" href="../images/favicon.png" />
+</head>
+
+<body>
+	<div id="backgroundMenu">
+		<div id="base"><img src="images/base.png"></div>
+		<div id="base_0"><img src="images/base_0.png"></div>
+		<div id="logo"><img src="images/logo.png"></div>
+		<div id="menu"><img src="images/menu.png"></div>
+		<div id="linha"><img src="images/linha.png"></div>
+	</div>
+	<div id="background">
+		<div id="selocompra">
+			<p class="esconde">Compre agora</p>
+			<img src="images/selocompra.png">
+		</div>
+		<div id="video">
+			<p class="esconde">Assista o vídeo</p>
+			<img src="images/video.png">
+		</div>
+		<div id="BonecaSFcopy"><img src="images/BonecaSFcopy.png"></div>
+		<div id="ttulocopy">
+			<h2 class="esconde">A brincadeira ficou mais gostosa!</h2>
+			<img src="images/ttulocopy.png">
+		</div>
+		<div id="garanta">
+			<p class="esconde">Garanta a sua! Estoque limitado.</p>
+			<img src="images/garanta.png">
+		</div>
+		<div id="geladeiragelasorveti">
+			<h3 class="esconde">Geladeira Gela Sorvetinho</h3>
+			<img src="images/geladeiragelasorveti.png">
+		</div>
+		<div id="puxe">
+			<p class="esconde">Puxe a casquinha...</p>
+			<img src="images/puxe.png">
+		</div>
+		<div id="aperte">
+			<p class="esconde">Aperte o botão...</p>
+			<img src="images/aperte.png">
+		</div>
+		<div id="osorvete">
+			<p class="esconde">O sorvete cai sobre a casquinha!</p>
+			<img src="images/osorvete.png">
+		</div>
+		<div id="chegou">
+			<h3 class="esconde">Chegou</h3>
+			<img src="images/chegou.png">
+		</div>
+	</div>
+	<div id="background2">
+		<div id="fundocopy2"><img src="images/fundocopy2.png"></div>
+		<div id="selocompra2"><img src="images/selocompra.png"></div>
+		<div id="Forma1"><img src="images/Forma1.png"></div>
+		<div id="Forma2"><img src="images/Forma2.png"></div>
+		<div id="Forma3"><img src="images/Forma3.png"></div>
+		<div id="Passo1">
+			<li class="esconde">Com a forminha limpa, coloque o suco de sua preferência.</li>
+			<img src="images/Passo1.png">
+		</div>
+		<div id="layer_1"><img src="images/layer_1.png"></div>
+		<div id="Passo2">
+			<li class="esconde">Encaixe as bases dos sorvetes na forminha e leve ao freezer por aproximadamente 2 horas ou até congelar.</li>
+			<img src="images/Passo2.png">
+		</div>
+		<div id="layer_2"><img src="images/layer_2.png"></div>
+		<div id="Passo3">
+			<li class="esconde">Hora de tirar o sorvetinho. Tire a forminha do freezer. Para desenformar o sorvetinho, molhe a forminha com pouca água.</li>
+			<img src="images/Passo3.png">
+		</div>
+		<div id="layer_3"><img src="images/layer_3.png"></div>
+		<div id="Aprenda">
+			<h3 class="esconde">Aprenda como fazer a bola de sorvete</h3>
+			<img src="images/Aprenda.png">
+		</div>
+		<div id="Ttulo">
+			<h2 class="esconde">E vem com forminha para fazer sorvete de verdade também!</h2>
+			<img src="images/Ttulo.png">
+		</div>
+	</div>
+	<div id="background3">
+		<div id="fundo3"><img src="images/fundo.png"></div>
+		<div id="fundo_03"><img src="images/fundo_0.png"></div>
+		<div id="Ondecomprar3">
+			<h2 class="esconde">Onde comprar?</h2>
+			<img src="images/Ondecomprar.png">
+		</div>
+		<div id="embalagem3"><img src="images/embalagem.png"></div>
+		<div id="VectorSmartObject3">
+			<h3 class="esconde">SWEER FANTASY Geladeira Gela Sorvetinho</h3>
+			<img src="images/VectorSmartObject.png">
+		</div>
+		<a id="ciatoy3"><img src="images/ciatoy.png"></a>
+		<a id="abelha3"><img src="images/abelha.png"></a>
+		<a id="cuba3"><img src="images/cuba.png"></a>
+		<a id="bumerang3"><img src="images/bumerang.png"></a>
+		<a id="superlegal3"><img src="images/superlegal.png"></a>
+	</div>
+	<div id="background4">
+		<div id="fundo4"><img src="images/fundo4.png"></div>
+		<div id="GelaSorvetinho">
+			<h3 class="esconde">Geladeira Gela Sorvetinho</h3>
+			<img src="images/GelaSorvetinho.png">
+		</div>
+		<div id="BonecaSF"><img src="images/BonecaSF.png"></div>
+		<div id="Face">
+			<p class="esconde">Facebook</p>
+			<img src="images/Face.png">
+		</div>
+		<div id="Insta">
+			<p class="esconde">Instagram</p>
+			<img src="images/Insta.png">
+		</div>
+		<div id="Formulario">
+			<p class="esconde">Preencha o formulário abaixo para receber as novidades CARDOSO TOYS em primeira mão!</p>
+			<img src="images/Formulario.png">
+		</div>
+		<div id="Cardoso">
+			<p class="esconde">Quero receber as novidades CARDOSO TOYS por Whatsapp</p>
+			<ul class="esconde">
+				<li>SAC - Ligação gratuita: 0800-7073676</li>
+				<li>Whatsapp: (11) 99948-1693</li>
+				<li>De 2ª a 6ª das 8h às 12h e das 13h às 16h (exceto feriados)</li>
+				<li>Rua Matashiro Yamaguishi, 278/390, Itaquera - São Paulo - SP - CEP 08260-040</li>
+				<li>CNPJ: 57.464.109/0001-05</li>
+			</ul>
+
+			<img src="images/Cardoso.png">
+		</div>
+		<div id="linha4"><img src="images/linha4.png"></div>
+		<div id="Parasabermais">
+			<p class="esconde">Para saber mais, cadastre-se e siga-nos pelas redes sociais.</p>
+			<img src="images/Parasabermais.png">
+		</div>
+	</div>
+</body>
+
+</html>
